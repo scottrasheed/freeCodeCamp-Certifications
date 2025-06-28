1a59be371817b82abcf02bcbceb8e7f573eb4d3d
Scott Rasheed <scottrasheed@ymail.com>
2025-06-28 15:38:33 -0400

    Update 01-palindrome-checker.js

--- 01-javascript-algorithms-dsa/01-palindrome-checker.js
+++ 01-javascript-algorithms-dsa/01-palindrome-checker.js
@@ -1,4 +1,9 @@
+// Palindrome Checker
+// Returns true if the input string is a palindrome
+
 function palindrome(str) {
   const cleaned = str.toLowerCase().replace(/[\W_]/g, '');
   return cleaned === cleaned.split('').reverse().join('');
 }
+
+console.log(palindrome("racecar")); // true
